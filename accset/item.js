e839ac81b81203d10829afd9e2262f772ef7abc9
vuongquangminh <vuongquangminh120802@gmail.com>
2023-04-17 21:59:16 +0700

    them san pham vao gio hang

--- accset/item.js
+++ accset/item.js
@@ -2,6 +2,9 @@
 
 
 // Thay doi anh
+const names = JSON.parse(localStorage.getItem('name'))
+
+
 var items = document.querySelectorAll('.contact-body-left .help img')
 console.log(items)
 items.forEach(function(item, index) {
@@ -41,18 +44,23 @@ sizes.forEach(function(item, index) {
 // them hang
 
 var quality = document.querySelector('.contact-body-right-quality p')
+
 var qualityNum = parseInt(quality.textContent)
+console.log(qualityNum)
 var listbtn = document.querySelectorAll('.contact-body-right-quality i')
 listbtn.forEach(function(btn, index) {
     btn.onclick = function() {
         if( index===0 ) {
             if(qualityNum==0) {
                 quality.innerHTML = `${qualityNum}`
+
+                
             }
             else{
                 qualityNum--
                 console.log(qualityNum)
                 quality.innerHTML = `${qualityNum}`
+
             }
 
         }
@@ -60,7 +68,10 @@ listbtn.forEach(function(btn, index) {
         if ( index===1 ) {
             qualityNum++
             quality.innerHTML = `${qualityNum}`
+
         }
+
+        
     }
 })
 
@@ -120,6 +131,134 @@ var modalBody = document.querySelector('#Cart .modal-body')
 modalBody.onclick = function (event) {
     event.stopPropagation()
 }
+
+
+
+
+//Them gio hang hien san pham trong Cart
+
+var addItem = document.querySelector('.contact-body-right-footer')
+const name = document.querySelector('.name').textContent
+const price = document.querySelector('.price').textContent
+var sumPrice = []
+setTimeout(() => {
+    addItem.onclick = () => {
+        
+        classmodal.style.display = 'block'
+        const soLuong = document.querySelector('.contact-body-right-quality p').textContent
+        console.log(soLuong)
+        const size = document.querySelector('.item.color').textContent //size
+        var list_item = document.querySelector('.have-item')
+        list_item.innerHTML = list_item.innerHTML + `
+        <div class= "wrapper-item">   
+            <div class="box-item">
+                <div class="have-item-head">
+                    ${name}
+                </div>
+                <div class="have-item-body">
+                    <div class="have-item-body-size">
+                        Size: ${size}
+                    </div>
+                    <div class="have-item-body-price">
+                        ${price}
+                    </div>
+                </div>
+                <div class="have-item-body-soluong">
+                    SL: 
+                    <i class="fa-solid fa-minus iconAdd"></i>
+                    <div>
+                        ${soLuong}
+                    </div>
+                    <i class="fa-solid fa-plus iconAdd"></i>
+                </div>
+            </div>
+
+            <i class="fa-solid fa-xmark closeX"></i>
+        </div> `
+          
+        
+        // const i = document.querySelector('.have-item-body-price').innerText
+        
+        // var iNum = Number(i)
+        // console.log(iNum)
+        // sumPrice.push(iNum)
+        // sumPrice.reduce((total, currentValue, currentIndex, arr) => {
+        //     console.log(total + currentValue)
+        // }, initialValue)
+        
+        
+        const closeXs = document.querySelectorAll(".closeX")
+        const deleteItem = document.querySelectorAll(".wrapper-item")
+
+        closeXs.forEach((closeX, index) => {
+            closeX.onclick = () => {
+                deleteItem[index].remove()
+            }
+        })
+
+        deleteItem.forEach(iteem => {
+            console.log(iteem)
+        })
+
+
+        var addSL = document.querySelector('.have-item-body-soluong div')
+        var addSLNum = parseInt(addSL.textContent)
+        const iconAdd = document.querySelectorAll('.iconAdd')
+        iconAdd.forEach((btn2, index) => {
+            btn2.onclick = function() {
+                if( index===0 ) {
+                    if(addSLNum==0) {
+                        addSL.innerHTML = `${addSLNum}`
+        
+                        
+                    }
+                    else{
+                        addSLNum--
+                        console.log(addSLNum)
+                        addSL.innerHTML = `${addSLNum}`
+
+                    }
+        
+                }
+        
+                if ( index===1 ) {
+                    addSLNum++
+                    addSL.innerHTML = `${addSLNum}`
+        
+                }
+        
+                
+            }
+            
+        })
+        
+
+       
+
+    }
+
+}, 0)
+
+const imgs= document.querySelectorAll('.imgs img')
+
+imgs.forEach( (img, index) => {
+    img.onclick = () => {
+        console.log(img)
+    }
+})
+
+
+const closeX = document.querySelectorAll(".closeX")
+const deleteItem = document.querySelectorAll(".wrapper-item")
+
+const openss = document.querySelector('#liveToastBtn')
+openss.onclick=() => {
+    document.querySelector('#liveToast').style.display='block'
+}
+console.log(openss)
+
+
+
 
 
 
