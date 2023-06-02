d6b59e25780d25d70bc695c54f855a1698b9951e
vuongquangminh <vuongquangminh120802@gmail.com>
2023-06-02 09:29:50 +0700

    load anh moi khi click

--- changesp.js
+++ changesp.js
@@ -1,12 +1,28 @@
+// Chuức năng khi click vào sản phẩm nào thì nó sẽ mang giá của sp đấy sang bên item
+
+
 var Sanphams = document.querySelectorAll('.contact2-columns-item')
 console.log(Sanphams)
+// document.querySelectorAll(".contact2-columns-item a").forEach(bb => {
+//     bb.addEventListener("click", function(event) {
+//         event.preventDefault();
+//         // Các đoạn mã khác bạn muốn thực hiện thay thế hành động mặc định
+//       });
+// }
+// ) 
 
 Sanphams.forEach(Sanpham => {
-    Sanpham.onclick = () => {
+    Sanpham.onclick = (e) => {
         var gia = parseInt(Sanpham.querySelector('.contact2-columns-item p').textContent)
         localStorage.setItem("PriceCurrent", gia);
         var NameCurrent = Sanpham.querySelector('.item-sub h6').textContent
         localStorage.setItem("NameCurrent", NameCurrent);
+        var New_Img_src = e.target.src
+        Avata_Src = '.' + New_Img_src.slice(21)
+        // console.log(Avata_Src)
+        localStorage.setItem("Avata_Src", Avata_Src);
+
+        
     }
 })
 // Lấy giá của LocalStorage xong rồi thêm chèn vào PlacePrice
@@ -19,3 +35,9 @@ PlacePrice.innerHTML=`${updatePrice}.000`
 var PlaceName = document.querySelector('.contact-body-right .name')
 var updateName = localStorage.getItem("NameCurrent")
 PlaceName.innerHTML = `${updateName}`
+
+// Lấy ảnh trong LocalStorage rồi chèn vào thẻ img sp
+var Show_Avt = document.querySelector('.contact-body-left .show img')
+var New_Avata_Src = localStorage.getItem("Avata_Src")
+Show_Avt.src = New_Avata_Src
+
